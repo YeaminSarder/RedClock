0c93089406d181970d777a59792b30361e269cd5
yeaminsarder <yeaminsardersr@gmail.com>
2025-11-10 21:48:37 +0600

    logging

---
+++ app/src/main/java/yeamin/sarder/redclock/MyClass.java
@@ -0,0 +1,11 @@
+package yeamin.sarder.redclock;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
+
+public class MyClass implements IXposedHookLoadPackage{
+    public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app: " + lpparam.packageName);
+    }
+}
