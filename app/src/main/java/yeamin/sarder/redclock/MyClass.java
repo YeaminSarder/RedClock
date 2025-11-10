a4a90620b0230bc226a91ac9a56248f23e9d247f
yeaminsarder <yeaminsardersr@gmail.com>
2025-11-11 00:45:10 +0600

    working

--- app/src/main/java/yeamin/sarder/redclock/MyClass.java
+++ app/src/main/java/yeamin/sarder/redclock/MyClass.java
@@ -1,11 +1,29 @@
 package yeamin.sarder.redclock;
 
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+
+import android.graphics.Color;
+import android.widget.TextView;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 
 public class MyClass implements IXposedHookLoadPackage{
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
+        XposedBridge.log("Loaded app2: " + lpparam.packageName);
+        if (!lpparam.packageName.equals("com.android.systemui")) return;
+        findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", boolean.class ,new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                TextView tv = (TextView) param.thisObject;
+                String text = tv.getText().toString();
+                tv.setText(text + " :)");
+                tv.setTextColor(Color.RED);
+
+            }
+        });
         XposedBridge.log("Loaded app: " + lpparam.packageName);
     }
 }
